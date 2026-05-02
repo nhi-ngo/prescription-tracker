3327a04f8838cdcbe894067e8759b70619a56152
nhi-ngo <nhingo.y@gmail.com>
2026-05-02 13:12:37 -0500

    created post and patch apis

--- server/routes/authorization.js
+++ server/routes/authorization.js
@@ -14,4 +14,44 @@ router.get('/', async (req, res) => {
 	}
 });
 
+// CREATE authorization
+router.post('/', async (req, res) => {
+	try {
+		const { patient_name, medication, status } = req.body;
+
+		const result = await pool.query(
+			`INSERT INTO authorizations
+      (patient_name, medication, status)
+      VALUES ($1,$2,$3)
+      RETURNING *`,
+			[patient_name, medication, status],
+		);
+
+		res.status(201).json(result.rows[0]);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).json({ error: 'Failed to create authorization' });
+	}
+});
+
+// UPDATE status
+router.patch('/:id', async (req, res) => {
+	try {
+		const { status } = req.body;
+
+		const result = await pool.query(
+			`UPDATE authorization
+      SET status = $1
+      WHERE id = $2
+      RETURNING *`,
+			[status, req.params.id],
+		);
+
+		res.json(result.rows[0]);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).json({ error: 'Failed to update status' });
+	}
+});
+
 module.exports = router;
